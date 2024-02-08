12f279b98ef8458ac44d810b2a81bf11ef441461
Tammiat <tamanateaminata08@gmail.com>
2024-02-08 15:10:03 +0100

    ajout deconnexion + variable user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -92,9 +92,10 @@ class UserController extends AbstractController
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        
+        
     }
-
-
+    
 }
 
 
